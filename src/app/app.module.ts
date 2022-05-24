dfc68d2dda2c5fd61c752d5373ad25b979124fa4
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2022-05-24 17:48:06 -0400

    ⚡️ Sprint: card without child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './exercises/card/card.component';
+import { CardChildComponent } from './exercises/card/card-child/card-child.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardComponent, CardChildComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/exercises/card/card.component.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface User {
+  id: number;
+  name: string;
+  posts: Post[];
+  isExpanded: boolean;
+}
+
+interface Post {
+  userId: number;
+  title: string;
+  body: string;
+}
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+  users: User[] = [] as User[];
+  posts: Post[] = [] as Post[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getPosts().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+      },
+      complete: () => {
+        this.getUsers().subscribe({
+          next: (users) => {
+            this.users = users.map((user) => ({
+              ...user,
+              posts: this.posts.filter((post) => post.userId === user.id),
+              isExpanded: false,
+            }));
+          },
+          complete: () => {
+            console.log("Daniel's Observable callback hell");
+          },
+        });
+      },
+    });
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  }
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  }
+
+  onClick(index: number) {
+    this.users[index].isExpanded = !this.users[index].isExpanded;
+  }
+}
